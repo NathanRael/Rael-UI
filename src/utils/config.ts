853e1ec12ab934e600788145fdb33c8000912922
Natanaël RALAIVOAVY <ralaivoavy.natanael@gmail.com>
2024-09-13 14:46:16 +0200

    feat: add default config file

---
+++ src/utils/config.ts
@@ -0,0 +1,11 @@
+// config.ts
+export const defaultConfig = {
+    colors: {
+        primary: '#3498db',  // Default primary color
+        secondary: '#95a5a6',
+        danger: '#e74c3c',
+    },
+    animation: {
+        enabled: true,        // Default animation state
+    },
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,15 @@
+import {defaultConfig} from "./src/utils/config";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          'primary' : defaultConfig.colors.primary,
+          'secondary' : defaultConfig.colors.secondary,
+          'danger' : defaultConfig.colors.danger,
+      }
+    },
   },
   plugins: [],
 };
