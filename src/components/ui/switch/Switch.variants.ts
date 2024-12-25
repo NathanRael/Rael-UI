da1ee2831ad2d6e37be1abf7b045a71e18eafdc1
NathanRael <ralaivoavy.natanael@gmail.com>
2024-12-25 18:05:00 +0300

    style(switch) : change switch color to match new defined color

--- src/components/ui/switch/Switch.variants.ts
+++ src/components/ui/switch/Switch.variants.ts
@@ -6,8 +6,8 @@ import {defaultVariants} from "@/components/default.ts";
 export const switchVariants = cva(`peer h-6 w-11 rounded-full border after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border  after:transition-all after:content-['']  peer-checked:after:translate-x-full  `, {
     variants: {
         variant : {
-            outline : 'border-input-outline-l-border after:border-input-outline-l-border peer-checked:after:bg-primary',
-            fill : 'bg-input-fill-l-bg after:bg-white border-input-fill-l-border after:border-gray-300 peer-checked:border-input-fill-l-bg  peer-checked:after:bg-primary peer-checked:after:border-white dark:after:border-bg-input-fill-bg dark:bg-input-fill-d-bg  dark:border-input-fill-d-bg dark:peer-checked:border-input-fill-d-bg dark:peer-checked:after:border-input-fill-d-bg ',
+            outline : 'border-neutral-light-60 after:border-neutral-light-60 peer-checked:after:bg-primary',
+            fill : 'bg-neutral-light-60 after:bg-white-100 border-neutral-light-60 after:border-neutral-light-60 peer-checked:border-neutral-light-60  peer-checked:after:bg-primary-100 peer-checked:after:border-white dark:after:border-neutral-dark-60 dark:bg-neutral-dark-60  dark:border-neutral-dark-60 dark:peer-checked:border-neutral-dark-60 dark:peer-checked:after:border-neutral-dark-60 ',
         },
         disabled: {
             true: 'after:bg-black after:border-black  cursor-not-allowed ',
