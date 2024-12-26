775b848e85e4cca21f4cffef06503864be1b2925
NathanRael <ralaivoavy.natanael@gmail.com>
2024-12-26 11:20:12 +0300

    refactor(tailwind.config.ts) : deleted old color styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,6 @@ export default {
     theme: {
         extend: {
             colors: {
-                // Base colors
                 'danger': colors.danger,
                 'dark': colors.dark,
                 primary: {
@@ -54,112 +53,6 @@ export default {
                     60: 'hsl(var(--color-neutral-light-60))',
                 }
                 ,
-
-// Component colors 
-
-                input: {
-                    fill: {
-                        d: {
-                            'bg':
-                                '#27272a',
-                            'placeholder':
-                                '#737373',
-                            'text':
-                            colors.white,
-                            'border':
-                            colors.black,
-                        }
-                        ,
-                        l: {
-                            'bg':
-                                '#e5e5e5',
-                            'placeholder':
-                                '#737373',
-                            'text':
-                            colors.black,
-                            'border':
-                            colors.white,
-
-                        }
-                    }
-                    ,
-                    outline: {
-                        d: {
-                            'bg':
-                            colors.black,
-                            'border':
-                                '#a3a3a3',
-                            'placeholder':
-                                '#a3a3a3',
-                            'text':
-                            colors.white,
-                        }
-                        ,
-                        l: {
-                            'bg':
-                            colors.white,
-                            'border':
-                                '#a3a3a3',
-                            'placeholder':
-                                '#737373',
-                            'text':
-                            colors.black,
-                        }
-                    }
-                }
-                ,
-                meta: {
-                    fill: {
-                        d: {
-                            'bg':
-                                '#18181b',
-                            'border':
-                                '#262626',
-                            'text':
-                            colors.white,
-                            'text-sec':
-                                '#9ca3af',
-                        }
-                        ,
-                        l: {
-                            'bg':
-                                '#fff',
-                            'border':
-                                '#e4e4e7',
-                            'text':
-                            colors.black,
-                            'text-sec':
-                                '#4b5563',
-                        }
-                        ,
-                    }
-                    ,
-                    outline: {
-                        d: {
-                            'bg':
-                            colors.black,
-                            'border':
-                                '#a3a3a3',
-                            'text':
-                            colors.white,
-                            'text-sec':
-                                '#6b7280',
-                        }
-                        ,
-                        l: {
-                            'bg':
-                            colors.white,
-                            'border':
-                                '#a3a3a3',
-                            'text':
-                            colors.black,
-                            'text-sec':
-                                '#6b7280',
-                        }
-                        ,
-                    }
-                    ,
-                }
             },
             fontSize: {
                 "title":
