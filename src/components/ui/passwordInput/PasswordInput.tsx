5ef0746cd405ba6ad678e3178d4b3354c247a794
NathanRael <ralaivoavy.natanael@gmail.com>
2024-12-26 09:36:20 +0300

    feat(passwordInput) : add ref & fix size variant not autocomplete

--- src/components/ui/passwordInput/PasswordInput.tsx
+++ src/components/ui/passwordInput/PasswordInput.tsx
@@ -1,15 +1,15 @@
 import {Eye, EyeOff} from "lucide-react";
-import {InputHTMLAttributes, MouseEventHandler, useState} from "react";
+import {forwardRef, MouseEventHandler, useImperativeHandle, useRef, useState} from "react";
 import {cn} from "@/utils/cn.ts";
 import {passwordInputVariants, realInputVariants} from "./PasswordInput.variants.ts";
 import {useComponentStyle} from "@/components/ui/ComponentStyle.Context.ts";
-import {Radius, Size, Variant} from "@/components/global.types.ts";
+import {VariantProps} from "class-variance-authority";
 
-type PasswordInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, "type"> & {
-    variant?: Variant;
-    size?: Size;
-    radius?: Radius;
-    block?: boolean;
+type PasswordInputProps = Omit<React.ComponentProps<'input'>, 'size'> & VariantProps<typeof passwordInputVariants> &  {
+    // variant?: Variant;
+    // size?: Size;
+    // radius?: Radius;
+    // block?: boolean;
     showIcon?: boolean;
     className? : string;
     leftContent?: React.ReactNode,
@@ -22,10 +22,13 @@ type ShowPasswordIconProps = {
     onClick? : MouseEventHandler;
 };
 
-const PasswordInput = ({variant, size, radius, block, className, showIcon = true, leftContent, rightContent, ...props}: PasswordInputProps) => {
+const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(({variant, size, radius, block, className, showIcon = true, leftContent, rightContent, ...props}, ref) => {
     const {cVariant} = useComponentStyle();
     const userProps = {variant : variant || cVariant, size, radius, block};
-    const [showPassword, setShowPassword] = useState(false)
+    const inputRef = useRef<HTMLInputElement | null>(null)
+    const [showPassword, setShowPassword] = useState(false);
+    
+    useImperativeHandle(ref, () =>  inputRef.current as HTMLInputElement, [inputRef])
     
     const handlePasswordIconClicked = () => {
         setShowPassword(prev => !prev)
@@ -34,12 +37,12 @@ const PasswordInput = ({variant, size, radius, block, className, showIcon = true
     return (
         <div tabIndex={-1} role="presentation" className={cn(passwordInputVariants(userProps), className)}>
             {leftContent}
-            <input  type={showPassword ? "text" : "password"}  {...props} className={cn(` secure ${realInputVariants({variant})}`, className)}/>
+            <input ref={inputRef}  type={showPassword ? "text" : "password"}  {...props} className={cn(` secure ${realInputVariants({variant})}`, className)}/>
             {showIcon && <ShowPasswordIcon onClick={handlePasswordIconClicked}  passwordShown={showPassword} className="cursor-pointer"/>}
             {rightContent}
         </div>
     )
-}
+});
 
 const ShowPasswordIcon = ({passwordShown, className, onClick}: ShowPasswordIconProps) => {
     return (passwordShown ? <EyeOff onClick={onClick} className={className}/> :
