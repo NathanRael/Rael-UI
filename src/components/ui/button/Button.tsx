651fe0b6c7b38bae2ff6248c50d8fcb7e6ae063c
NathanRael <ralaivoavy.natanael@gmail.com>
2024-12-26 09:36:36 +0300

    feat(button) : add ref

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,10 +1,10 @@
 import {LoaderCircle} from "lucide-react";
-import {ButtonHTMLAttributes} from "react";
-import {cn} from "../../../utils/cn.ts";
+import {ComponentProps, forwardRef} from "react";
+import {cn} from "@/utils";
 import {buttonVariants, LoaderVariants} from "./Button.variants.ts";
 import {ButtonVariant, Radius, Size} from "@/components/global.types.ts";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+type ButtonProps = ComponentProps<'button'> & {
     variant?: ButtonVariant,
     size?: Size,
     radius?: Radius,
@@ -13,16 +13,16 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
     disabled?: boolean,
 }
 
-const Button = ({variant, size, radius, loading, disabled, className,block, children, onClick, ...props}: ButtonProps) => {
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({variant, size, radius, loading, disabled, className,block, children, onClick, ...props}, ref) => {
 
     const userProps = {variant, size, radius, loading, disabled, block};
 
     return (
-        <button disabled={disabled} onClick={onClick} className={cn(buttonVariants(userProps), className)} {...props}>
+        <button ref={ref} disabled={disabled} onClick={onClick} className={cn(buttonVariants(userProps), className)} {...props}>
             {loading && <LoaderCircle  className={LoaderVariants({size})}/>}
             {children}
         </button>)
-}
+})
 
 export default Button
 
